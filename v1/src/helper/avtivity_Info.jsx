8e0ba1754af0eecf1c5038c8571acd13bc518cff
koraykoksal <kksl.koray@gmail.com>
2023-11-30 11:21:45 +0300

    up

--- v1/src/helper/avtivity_Info.jsx
+++ v1/src/helper/avtivity_Info.jsx
@@ -6,33 +6,10 @@ export const activityInfo = [
     {
         id:uid(),
         name: 'Çok Güzel Hareketler 2',
-        date: '12.12.2023',
+        date: '10.12.2023',
         image:cokguzelhareketler2,
         status: 'Aktif'
 
     },
-    {
-        id:uid(),
-        name: 'Çok Güzel Hareketler 3',
-        date: '12.12.2023',
-        image:cokguzelhareketler2,
-        status: 'Aktif'
 
-    },
-    {
-        id:uid(),
-        name: 'Çok Güzel Hareketler 4',
-        date: '12.12.2023',
-        image:cokguzelhareketler2,
-        status: 'Aktif'
-
-    },
-    {
-        id:uid(),
-        name: 'Çok Güzel Hareketler 5',
-        date: '12.12.2023',
-        image:cokguzelhareketler2,
-        status: 'Aktif'
-
-    }
 ]
